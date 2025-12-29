c058ccb47a1b028c173de4d0599071fe63f83238
Antony <apalaciosc0109@gmail.com>
2025-12-28 21:47:41 -0500

    docs(goal): add openApi doc to Goal records

--- src/main/java/pe/com/edu/prismaapp/prisma/dto/GoalApi.java
+++ src/main/java/pe/com/edu/prismaapp/prisma/dto/GoalApi.java
@@ -1,20 +1,61 @@
 package pe.com.edu.prismaapp.prisma.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import pe.com.edu.prismaapp.prisma.entities.Goal;
 
 public interface GoalApi {
-    record Create(@NotNull Long examId,
-                  Long studentId,
-                  double scoreGoal){}
 
-    record Update(@NotNull Long id,
-                  double scoreGoal){}
+    @Schema(name = "CreateGoalRequest", description = "Datos requeridos para crear una meta")
+    record Create(
+            @NotNull
+            @Schema(description = "Id del examen", example = "3")
+            Long examId,
 
-    record Response( @NotNull Long id,
-                     @NotNull Long examId,
-                     @NotNull Long studentId,
-                     double scoreGoal){
+            @NotNull
+            @Schema(description = "Id del alumno", example = "25")
+            Long studentId,
+
+            @NotNull
+            @Size(max = 1)
+            @Schema(description = "Porcentaje de mejora", example = "0.15")
+            double scoreGoal
+    ) {
+    }
+
+    @Schema(name = "UpdateGoalRequest", description = "Datos requeridos para actualizar una meta")
+    record Update(
+            @NotNull
+            @Schema(description = "Id de la meta", example = "3")
+            Long id,
+
+            @NotNull
+            @Size(max = 1)
+            @Schema(description = "Porcentaje de mejora", example = "0.15")
+            double scoreGoal
+    ) {
+    }
+
+    @Schema(name = "GoalResponse", description = "Respuesta con detalles de la meta")
+    record Response(
+            @NotNull
+            @Schema(description = "Id de la meta", example = "3")
+            Long id,
+
+            @NotNull
+            @Schema(description = "Id del examen", example = "3")
+            Long examId,
+
+            @NotNull
+            @Schema(description = "Id del alumno", example = "25")
+            Long studentId,
+
+            @NotNull
+            @Size(max = 1)
+            @Schema(description = "Porcentaje de mejora", example = "0.15")
+            double scoreGoal
+    ) {
 
         public static GoalApi.Response from(Goal goal) {
             return new GoalApi.Response(
